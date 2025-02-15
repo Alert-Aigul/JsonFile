99bde27e8e83b1d156f7f5a8b241f69fb514916b
Alert Aigul <alertaigul@gmail.com>
2025-02-15 21:43:13 +0200

    rename

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(f'easy_json_file/__init__.py') as fp:
                          fp.read()).group().replace("'", '')
 
 setup(
-    name="json_file",
+    name="easy_json_file",
     version=__version__,
     url="https://github.com/Alert-Aigul/JsonFile",
     download_url="https://github.com/Alert-Aigul/JsonFile/archive/refs/heads/main.zip",
